e30901aada689b8c220db893d5caa9403fc2ab6c
Mahmoud822004 <mahmoudhisham822004@gmail.com>
2024-09-20 22:33:36 +0300

    data

--- New JS/test.js
+++ New JS/test.js
@@ -1,6 +1,29 @@
-const x = Math.random()*10;
+/*function getBMI(mass, height){
+    let BMI;
+    BMI = mass/height**2;
+    return BMI;
 
-console.log(Math.floor(x));
-console.log(Math.ceil(x));
-console.log(Math.trunc(x));
-console.log(x);
+}
+
+const M_mass = 78;
+const M_height = 1.69;
+
+const J_mass = 2;
+const J_height = 1.95;
+
+const Mark_BMI = getBMI(M_mass, M_height);
+const John_BMI = getBMI(J_mass, J_height);
+
+Mark_BMI>John_BMI?console.log("Mark has higher BMI"):console.log("John has higher BMI");
+*/
+total=[];
+function CalcTip(bill){
+    bill>50 && bill<300? total.push(bill*=0.15):total.push(bill*=0.20); 
+}
+
+CalcTip(260);
+CalcTip(300);
+CalcTip(700);
+CalcTip(680);
+CalcTip(540);
+console.log(total);
