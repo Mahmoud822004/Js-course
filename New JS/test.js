d2eca29da120115d4fe690c01dcdaed04247fa77
Mahmoud822004 <mahmoudhisham822004@gmail.com>
2024-09-20 22:54:44 +0300

    data

--- New JS/test.js
+++ New JS/test.js
@@ -16,9 +16,12 @@ const John_BMI = getBMI(J_mass, J_height);
 
 Mark_BMI>John_BMI?console.log("Mark has higher BMI"):console.log("John has higher BMI");
 */
+tip=[];
 total=[];
 function CalcTip(bill){
-    bill>50 && bill<300? total.push(bill*=0.15):total.push(bill*=0.20); 
+    bill>50 && bill<300? tip.push(bill*=0.15):tip.push(bill*=0.20); 
+
+    bill>50 && bill<300? total.push(bill=bill+(bill*0.15)):total.push(bill=bill+(bill*0.20)); 
 }
 
 CalcTip(260);
@@ -26,4 +29,7 @@ CalcTip(300);
 CalcTip(700);
 CalcTip(680);
 CalcTip(540);
+console.log(tip);
 console.log(total);
+
+
