9f87da49526497d95879da82cda3e4e1359d6142
Mahmoud822004 <mahmoudhisham822004@gmail.com>
2024-09-21 18:40:05 +0300

    data

--- New JS/test.js
+++ New JS/test.js
@@ -16,6 +16,8 @@ const John_BMI = getBMI(J_mass, J_height);
 
 Mark_BMI>John_BMI?console.log("Mark has higher BMI"):console.log("John has higher BMI");
 */
+
+/*
 tip=[];
 total=[];
 function CalcTip(bill){
@@ -31,5 +33,38 @@ CalcTip(680);
 CalcTip(540);
 console.log(tip);
 console.log(total);
+*/
+
+ bills=[22,295,176,440,37,105,10,1100,86,52];
 
 
+ tips=[];
+ totals=[];
+
+function CalcTip(bill){
+    bill>50 && bill<300? tips.push(bill*0.15):tips.push(bill*0.20); 
+
+    bill>50 && bill<300? totals.push(bill=bill+(bill*0.15)):totals.push(bill=bill+(bill*0.20)); 
+}
+
+    for(let i=0; i<bills.length;i++){
+        CalcTip(bills[i]);
+ 
+    }
+
+console.log("Bills =",bills);
+console.log("Tips =",tips);
+console.log("Total =",totals);
+
+
+function calcAvarage(arr){
+    var sum=0;
+    for(i=0;i<arr.length;i++){
+        sum=arr[i]+sum;
+    }
+
+    let avarage = sum/arr.length;
+    return avarage;
+}
+
+console.log("Total avarage =",calcAvarage(totals));
